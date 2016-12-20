34cfcd29e970d16a3bd3b633500d94047fb27c49
Barrelrolla <chetkara@gmail.com>
2016-12-20 13:57:28 +0200

    fix tslint errors

--- server.ts
+++ server.ts
@@ -5,23 +5,23 @@ import * as mongojs from "mongojs"
 import { indexRouter } from "./routes/index"
 import { heroesRouter } from "./routes/heroes"
 
-var db = mongojs("mongodb://pesho:pesho@ds139428.mlab.com:39428/hearthstone-deck-builder")
+let db = mongojs("mongodb://pesho:pesho@ds139428.mlab.com:39428/hearthstone-deck-builder")
 
-var port = 3000
+let port = 3000
 
-var app = express()
+let app = express()
 
-app.set('views', path.join(__dirname), 'views')
-app.set('view engine', 'pug')
+app.set("views", path.join(__dirname), "views")
+app.set("view engine", "pug")
 
 app.use(express.static(path.join(__dirname)))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/', indexRouter)
-app.use('/heroes', heroesRouter)
+app.use("/", indexRouter)
+app.use("/heroes", heroesRouter)
 
 app.listen(port, function() {
-    console.log('Server listening on port ' + port)
+    console.log("Server listening on port " + port)
 })
