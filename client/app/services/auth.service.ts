b7f82ca7a06aa0e35b2b8cceca110617341c5e72
Barrelrolla <chetkara@gmail.com>
2016-12-23 14:51:14 +0200

    Implemented authentication

--- client/app/services/auth.service.ts
+++ client/app/services/auth.service.ts
@@ -7,7 +7,7 @@ declare var Auth0Lock: any;
 @Injectable()
 export class Auth {
   // Configure Auth0
-  lock = new Auth0Lock('1SbC4OLPjSngNFDTVGRd9fwyZY8Kg77v', 'barrelrolla.eu.auth0.com', {});
+  lock = new Auth0Lock('M5Bl1Zyq0Gzm6NPvMuLQz2h09KtaFPrN', 'barrelrolla.eu.auth0.com', {});
 
   constructor() {
     // Add callback for lock `authenticated` event
