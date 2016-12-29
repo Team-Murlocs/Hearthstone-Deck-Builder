923f247f44c193be2622f86819af4eb33d60e9a6
Barrelrolla <chetkara@gmail.com>
2016-12-29 15:24:48 +0200

    added auth guard

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpModule } from "@angular/http";
 import { FormsModule } from "@angular/forms";
 import { AUTH_PROVIDERS } from "angular2-jwt";
 import { Auth } from "./services/auth.service";
+import { AuthGuard } from "./services/auth.guard"
 import * as Components from "./component-loader";
 import { routing} from "./app.router";
 import { APP_BASE_HREF } from "@angular/common";
@@ -25,7 +26,7 @@ import { CardService } from "./services/card.service";
         ], // declare all derectives and components
     bootstrap: [Components.AppComponent], // root component to bootstarp
      providers: [{ provide: APP_BASE_HREF, useValue: '/' }, AUTH_PROVIDERS, //  appRoutingProviders,
-        Auth, Requester, CardService]
+        Auth, AuthGuard, Requester, CardService]
 })
 export class AppModule { }
 
--- client/app/app.router.ts
+++ client/app/app.router.ts
@@ -1,12 +1,13 @@
 import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { AuthGuard } from "./services/auth.guard"
 import * as Components from "./component-loader"
 
 const appRoutes = [
     { path: "", component: Components.HomeComponent, pathMatch: "full" },
     { path: "home", component: Components.HomeComponent },
     { path: "heroes", component: Components.HeroesComponent },
-    { path: "profile", component: Components.ProfileComponent },
+    { path: "profile", component: Components.ProfileComponent, canActivate: [AuthGuard] },
     { path: "cards/:type", component: Components.CardsComponent },
     { path: "cards/name/:name", component: Components.CardInfoComponent },
     { path: "decks", component: Components.DecksComponent },
